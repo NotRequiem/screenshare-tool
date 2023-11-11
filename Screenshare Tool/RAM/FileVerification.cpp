c84178d343b796ff04cce12306bb239cecf29182
Requiem <114197630+NotRequiem@users.noreply.github.com>
2023-11-11 11:58:44 +0100

    Delete Screenshare Tool directory

--- Screenshare Tool/RAM/FileVerification.cpp
+++
@@ -1,8 +0,0 @@
-#include "FileVerification.hpp"
-#include "TrustVerifyWrapper.hpp"
-
-// Checks if the digital signature of a file is valid (official) or spoofed/not valid
-bool IsFileSignatureValid(const std::wstring& filePath) {
-    TrustVerifyWrapper wrapper;
-    return wrapper.VerifyFileSignature(filePath);
-}
--- Screenshare Tool/RAM/FileVerification.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <string>
-
-bool IsFileSignatureValid(const std::wstring& filePath);
