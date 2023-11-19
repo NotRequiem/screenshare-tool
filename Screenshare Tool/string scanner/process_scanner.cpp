27d104ba93e2eaad9c56a510e7f9543b5d051d0b
Requiem <114197630+NotRequiem@users.noreply.github.com>
2023-11-19 22:34:23 +0100

    Update process_scanner.cpp

--- Screenshare Tool/string scanner/process_scanner.cpp
+++ Screenshare Tool/string scanner/process_scanner.cpp
@@ -1,22 +1,123 @@
-#include "process_scanner.hpp"
+#include "process_scanner.hpp" // TEST VERSION //
 
-bool isUnicodeCharacter(wchar_t ch) {
-    return iswprint(ch) != 0;
+struct ProcessParams {
+    const wchar_t* processName;
+    std::wstring searchPattern;
+    bool useRegex;
+};
+
+class ProcessScanException : public std::exception {
+public:
+    ProcessScanException(const std::wstring& message)
+        : message_(message), utf8Message_(ConvertToUtf8(message)) {}
+
+    const char* what() const noexcept override {
+        return utf8Message_.c_str();
+    }
+
+private:
+    std::wstring message_;
+    std::string utf8Message_;
+
+    std::string ConvertToUtf8(const std::wstring& wideStr) const {
+        int bufferSize = WideCharToMultiByte(CP_UTF8, 0, wideStr.c_str(), -1, nullptr, 0, nullptr, nullptr);
+        std::string utf8Message(bufferSize, 0);
+        WideCharToMultiByte(CP_UTF8, 0, wideStr.c_str(), -1, utf8Message.data(), bufferSize, nullptr, nullptr);
+        return utf8Message;
+    }
+};
+
+constexpr DWORD PROCESS_ACCESS_FLAGS = PROCESS_ALL_ACCESS;
+constexpr BOOL INVALID_HANDLE_VALUE_FALSE = FALSE;
+
+class ProcessSnapshot {
+public:
+    explicit ProcessSnapshot(DWORD flags) : handle_(CreateToolhelp32Snapshot(flags, 0)) {
+        if (handle_ == INVALID_HANDLE_VALUE) {
+            throw ProcessScanException(L"Failed to create a process snapshot. Error: " + std::to_wstring(GetLastError()));
+        }
+    }
+
+    ~ProcessSnapshot() {
+        if (handle_ != INVALID_HANDLE_VALUE) {
+            CloseHandle(handle_);
+        }
+    }
+
+    HANDLE GetHandle() const {
+        return handle_;
+    }
+
+private:
+    HANDLE handle_;
+};
+
+class ProcessHandle {
+public:
+    explicit ProcessHandle(HANDLE handle) : handle_(handle) {
+        if (handle_ == INVALID_HANDLE_VALUE) {
+            throw ProcessScanException(L"Invalid process handle.");
+        }
+    }
+
+    ~ProcessHandle() {
+        if (handle_ != INVALID_HANDLE_VALUE) {
+            CloseHandle(handle_);
+        }
+    }
+
+    HANDLE GetHandle() const {
+        return handle_;
+    }
+
+private:
+    HANDLE handle_;
+};
+
+void EnableDebugPrivilege() {
+    HANDLE hToken;
+    TOKEN_PRIVILEGES tp{};
+
+    if (OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken)) {
+        if (LookupPrivilegeValue(nullptr, SE_DEBUG_NAME, &tp.Privileges[0].Luid)) {
+            tp.PrivilegeCount = 1;
+            tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+            if (!AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), nullptr, nullptr)) {
+                throw ProcessScanException(L"AdjustTokenPrivileges failed with error code: " + std::to_wstring(GetLastError()));
+            }
+        }
+        else {
+            throw ProcessScanException(L"LookupPrivilegeValue failed with error code: " + std::to_wstring(GetLastError()));
+        }
+
+        CloseHandle(hToken);
+    }
+    else {
+        throw ProcessScanException(L"OpenProcessToken failed with error code: " + std::to_wstring(GetLastError()));
+    }
 }
 
+void scanMemoryRegion(HANDLE hProcess, const MEMORY_BASIC_INFORMATION& mbi, const std::wstring& processName, DWORD pid, const std::wstring& searchPattern, bool useRegex, size_t startIndex);
+
 void scanProcessStrings(const wchar_t* processName, const std::wstring& searchPattern, bool useRegex) {
-    DWORD pid = 0;
-    PROCESSENTRY32W pe32{};
-    pe32.dwSize = sizeof(PROCESSENTRY32W);
-
-    HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-
-    if (hSnapshot == INVALID_HANDLE_VALUE) {
-        std::wcerr << L"Failed to create a process snapshot to detect strings. Error: " << GetLastError() << std::endl;
+    if (processName == nullptr) {
+        std::wcerr << L"Invalid processName parameter.\n";
         return;
     }
 
-    // Check if the process is running
+    if (searchPattern.empty()) {
+        std::wcerr << L"Invalid searchPattern parameter.\n";
+        return;
+    }
+
+    DWORD pid = 0;
+    ProcessSnapshot snapshot(TH32CS_SNAPPROCESS);
+    HANDLE hSnapshot = snapshot.GetHandle();
+
+    PROCESSENTRY32W pe32{};
+    pe32.dwSize = sizeof(PROCESSENTRY32W);
+
     if (Process32FirstW(hSnapshot, &pe32)) {
         do {
             if (lstrcmpiW(pe32.szExeFile, processName) == 0) {
@@ -27,138 +128,92 @@ void scanProcessStrings(const wchar_t* processName, const std::wstring& searchPa
         } while (Process32NextW(hSnapshot, &pe32));
     }
 
-    CloseHandle(hSnapshot);
-
-    // If the process is not running, skip the memory scanning
     if (pid == 0) {
-        std::wcout << L"[Process Scanner] Process not running: " << processName << std::endl;
+        std::wcout << L"[Process Scanner] Process not running: " << processName << '\n';
         return;
     }
 
-    // Open the process with PROCESS_ALL_ACCESS access
-    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-
-    if (hProcess == NULL) {
-        std::wcerr << L"Failed to open the process. Error: " << GetLastError() << std::endl;
-        return;
-    }
-
-    // Buffer to read process memory
-    wchar_t buffer[4096]{};
-
-    // Size of the candidate buffer
-    const size_t candidateSize = 110;
-
-    // Candidate buffer to store potential file paths
-    wchar_t* candidate = new wchar_t[candidateSize];
+    ProcessHandle processHandle(OpenProcess(PROCESS_ACCESS_FLAGS, INVALID_HANDLE_VALUE_FALSE, pid));
+    HANDLE hProcess = processHandle.GetHandle();
 
     MEMORY_BASIC_INFORMATION mbi;
     wchar_t* address = 0;
 
-    // Iterate through the memory regions of the process
-    while (VirtualQueryEx(hProcess, address, &mbi, sizeof(mbi)) != 0) {
-        if (mbi.State == MEM_COMMIT) {
-            SIZE_T bytesRead;
-
-            // Check if address is not null before reading process memory
-            if (address != nullptr) {
-                // Read the process memory into the buffer
-                if (ReadProcessMemory(hProcess, address, buffer, mbi.RegionSize, &bytesRead)) {
-                    size_t startIndex = 0;
-
-                    // Iterate through the buffer and identify potential file paths
-                    for (size_t i = 0; i < bytesRead / sizeof(wchar_t); i++) {
-                        if (!isUnicodeCharacter(buffer[i])) {
-                            int candidateIndex = 0;
-
-                            // Copy the potential file path to the candidate buffer
-                            for (size_t j = startIndex; j <= i; j++) {
-                                candidate[candidateIndex++] = buffer[j];
-                            }
-
-                            candidate[candidateIndex] = L'\0';
-
-                            if (useRegex) {
-                                // Convert the search pattern to a regex
-                                std::wregex pattern(searchPattern);
-
-                                // Use regex match for the entire buffer
-                                if (std::regex_search(std::wstring(candidate), pattern)) {
-                                    // Process the candidate string as needed
-                                    std::wcout << L"String detected in " << processName << L" (PID: " << pid << L"): " << std::wstring(candidate) << std::endl;
-                                }
-                            }
-                            else {
-                                std::wstring candidateString(candidate);
-                                // Search for the exact string without using regex
-                                size_t found = candidateString.find(searchPattern);
-                                if (found != std::wstring::npos) {
-                                    // Process the candidate string as needed
-                                    std::wcout << L"String detected in " << processName << L" (PID: " << pid << L"): " << candidateString << std::endl;
-                                }
-                            }
-
-                            startIndex = i + 1;  // Move to the next character after the detected string
-                        }
-                    }
-                }
-                else {
-                    // Print an error message including the address and continue to the next region
-                    std::wcerr << L"ReadProcessMemory failed. Error: " << GetLastError() << L", Address: " << address << std::endl;
-                }
+    while (VirtualQueryEx(hProcess, reinterpret_cast<LPCVOID>(address), &mbi, sizeof(mbi)) != 0) {
+        if ((mbi.Protect & PAGE_GUARD) != PAGE_GUARD) {
+            if (mbi.State == MEM_COMMIT) {
+                scanMemoryRegion(hProcess, mbi, processName, pid, searchPattern, useRegex, 0);
             }
-
-            address += mbi.RegionSize;
         }
-    }
 
-    delete[] candidate;
-    CloseHandle(hProcess);
+        address += mbi.RegionSize;
+    }
 }
 
-void EnableDebugPrivilege() {
-    HANDLE hToken;
-    TOKEN_PRIVILEGES tp{};
+bool isUnicodeCharacter(wchar_t ch) {
+    return iswprint(ch) != 0;
+}
 
-    if (OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken)) {
-        if (LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &tp.Privileges[0].Luid)) {
-            tp.PrivilegeCount = 1;
-            tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+void scanMemoryRegion(HANDLE hProcess, const MEMORY_BASIC_INFORMATION& mbi, const std::wstring& processName, DWORD pid, const std::wstring& searchPattern, bool useRegex, size_t startIndex) {
+    SIZE_T bytesRead;
+    std::vector<wchar_t> candidate(mbi.RegionSize / sizeof(wchar_t));
 
-            if (!AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), NULL, NULL)) {
-                std::cerr << "AdjustTokenPrivileges failed with error code: " << GetLastError() << std::endl;
+    if (startIndex >= candidate.size()) {
+        std::wcerr << L"Invalid startIndex parameter." << std::endl;
+        return;
+    }
+
+    if (mbi.BaseAddress != nullptr) {
+        if (ReadProcessMemory(hProcess, mbi.BaseAddress, candidate.data(), mbi.RegionSize, &bytesRead)) {
+            if (startIndex >= bytesRead / sizeof(wchar_t)) {
+                std::wcerr << L"Invalid startIndex parameter." << std::endl;
+                return;
+            }
+
+            for (auto it = candidate.begin() + startIndex; it != candidate.begin() + bytesRead / sizeof(wchar_t); ++it) {
+                if (!isUnicodeCharacter(*it)) {
+                    auto start = candidate.begin() + startIndex;
+                    auto end = it;
+                    std::wstring candidateString(start, end);
+
+                    if (useRegex) {
+                        std::wregex pattern(searchPattern);
+                        if (std::regex_search(candidateString, pattern)) {
+                            std::wcout << L"String detected in " << processName << L" (PID: " << pid << L"): " << candidateString << std::endl;
+                        }
+                    }
+                    else {
+                        size_t found = candidateString.find(searchPattern);
+                        if (found != std::wstring::npos) {
+                            std::wcout << L"String detected in " << processName << L" (PID: " << pid << L"): " << candidateString << std::endl;
+                        }
+                    }
+
+                    startIndex = std::distance(candidate.begin(), it) + 1;
+                }
             }
         }
         else {
-            std::cerr << "LookupPrivilegeValue failed with error code: " << GetLastError() << std::endl;
+            std::wcerr << L"ReadProcessMemory failed. Error: " << GetLastError() << L", Address: " << mbi.BaseAddress << std::endl;
         }
-
-        CloseHandle(hToken);
-    }
-    else {
-        std::cerr << "OpenProcessToken failed with error code: " << GetLastError() << std::endl;
     }
 }
 
 int main() {
     EnableDebugPrivilege();
 
-    std::vector<std::tuple<const wchar_t*, std::wstring, bool>> processParameters = {
-        std::make_tuple(L"lghub_agent.exe", L"durationms.+\"isDown\"", true),
-        std::make_tuple(L"Razer Synapse.exe", L"DeleteMacroEvent", false),
-        std::make_tuple(L"Razer Synapse 3.exe", L"SetKeysPerSecond", false),
-        std::make_tuple(L"RazerCentralService.exe", L"Datasync: Status: COMPLETE Action: NONE Macros/", false),
-        std::make_tuple(L"SteelSeriesGGClient.exe", L"delay.+is_deleted", true),
-        std::make_tuple(L"Onikuma.exe", L"LeftKey CODE:", false),
-        std::make_tuple(L"explorer.exe", L"^file:///.+?.exe", true)
+    std::vector<ProcessParams> processParameters = {
+        { L"lghub_agent.exe", L"durationms.+\"isDown\"", true },
+        { L"Razer Synapse.exe", L"DeleteMacroEvent", false },
+        { L"Razer Synapse 3.exe", L"SetKeysPerSecond", false },
+        { L"RazerCentralService.exe", L"Datasync: Status: COMPLETE Action: NONE Macros/", false },
+        { L"SteelSeriesGGClient.exe", L"delay.+is_deleted", true },
+        { L"Onikuma.exe", L"LeftKey CODE:", false },
+        { L"explorer.exe", L"^file:///.+?.exe", true }
     };
 
     for (const auto& params : processParameters) {
-        const wchar_t* processName = std::get<0>(params);
-        std::wstring searchPattern = std::get<1>(params);
-        bool useRegex = std::get<2>(params);
-
-        scanProcessStrings(processName, searchPattern, useRegex);
+        scanProcessStrings(params.processName, params.searchPattern, params.useRegex);
     }
 
     return 0;
