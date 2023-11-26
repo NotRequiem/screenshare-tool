b41761039772094ed336d7d84f2bb83094569d18
Requiem <114197630+NotRequiem@users.noreply.github.com>
2023-11-26 03:12:18 +0100

    Update mouse.cpp

--- Screenshare Tool/devices/mouse.cpp
+++ Screenshare Tool/devices/mouse.cpp
@@ -1,9 +1,9 @@
-#include "..\gui\color.hpp"
+include "..\gui\color.hpp"
 #include "mouse.hpp"
 
-static bool MouseVidAndPid() {
+bool MouseCheck() {
     Console::SetColor(ConsoleColor::BrightGreen, ConsoleColor::Black);
-    std::wcout << "[Device Scanner] Analyzing mice's Vendor ID and Product ID..." << std::endl;
+    std::wcout << "[Device Scanner] Retrieving mice's VID and PID..." << std::endl;
     Console::ResetColor();
 
     HRESULT hr = CoInitializeEx(0, COINIT_MULTITHREADED);
@@ -116,7 +116,7 @@ static bool MouseVidAndPid() {
 
         hr = pclsObj->Get(L"DeviceID", 0, &vtProp, 0, 0);
         if (SUCCEEDED(hr)) {
-            wprintf(L"User's mice VID and PID: %s\n", vtProp.bstrVal);
+            wprintf(L"User's mouse VID and PID: %s\n", vtProp.bstrVal);
             VariantClear(&vtProp);
         }
 
@@ -132,48 +132,4 @@ static bool MouseVidAndPid() {
     CoUninitialize();
 
     return true;
-}
-
-// External function to check the number of connected mice
-void MouseCheck() {
-    Console::SetColor(ConsoleColor::BrightGreen, ConsoleColor::Black);
-    std::wcout << "[Device Scanner] Checking if more than one mice is plugged... " << std::endl;
-    Console::ResetColor();
-    MouseVidAndPid();
-
-    UINT numDevices = 0;
-
-    // First call to get the number of devices
-    if (GetRawInputDeviceList(nullptr, &numDevices, sizeof(RAWINPUTDEVICELIST)) == -1) {
-        std::cerr << "Failed checking if more than one mice were plugged into the computer." << std::endl;
-        return;
-    }
-
-    RAWINPUTDEVICELIST* rawInputDeviceList = new RAWINPUTDEVICELIST[numDevices];
-
-    // Second call to get the actual device list
-    if (GetRawInputDeviceList(rawInputDeviceList, &numDevices, sizeof(RAWINPUTDEVICELIST)) == -1) {
-        std::cerr << "Failed checking if more than one mice were plugged into the computer." << std::endl;
-        delete[] rawInputDeviceList;
-        return;
-    }
-
-    int mouseCount = 0;
-
-    if (rawInputDeviceList == nullptr) {
-        std::cerr << "Failed checking if more than one mice were plugged into the computer." << std::endl;
-    }
-
-    for (UINT i = 0; i < numDevices; ++i) {
-        #pragma warning(suppress: 6385)
-        if (rawInputDeviceList[i].dwType == RIM_TYPEMOUSE) {
-            ++mouseCount;
-        }
-    }
-
-    delete[] rawInputDeviceList;
-
-    if (mouseCount > 1) {
-        std::cout << "More than one mice is connected. This is bannable." << std::endl;
-    }
 }
