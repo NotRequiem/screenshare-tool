ca74a00bc292bdfa959f12ec96bd1c78e4d658a3
Requiem <114197630+NotRequiem@users.noreply.github.com>
2023-12-09 01:52:35 +0100

    Add files via upload
    Signed-off-by: Requiem <114197630+NotRequiem@users.noreply.github.com>

---
+++ Screenshare Tool/checks/bam/bam.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <Windows.h>
+#include <iostream>
+#include <memory>
+#include <map>
+#include <string>
+#include <Wbemidl.h>
+#include <comdef.h>
+
+#include "..\..\miscellaneous\gui\color.h"
+#include "..\..\miscellaneous\digital signature\trustverify.hpp"
+
+#pragma comment(lib, "wbemuuid.lib")
+
+void bam();
---
+++ Screenshare Tool/checks/eventlog/evtquery.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <windows.h>
+#include <winevt.h>
+#include <iostream>
+#include <vector>
+#include <memory>
+
+#include "..\..\miscellaneous\gui\color.h"
+
+#pragma comment(lib, "wevtapi.lib")
+
+void SystemTimeChange();
