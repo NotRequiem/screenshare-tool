23739565c932651c34ebc95b7477470582238289
Requiem <114197630+NotRequiem@users.noreply.github.com>
2023-12-10 20:47:51 +0100

    Update devices.cpp
    Signed-off-by: Requiem <114197630+NotRequiem@users.noreply.github.com>

--- Screenshare Tool/checks/devices/devices.cpp
+++ Screenshare Tool/checks/devices/devices.cpp
@@ -1,21 +1,152 @@
 #include "devices.hpp"
 
-// Function to retrieve the system's last boot time
-static void LastComputerBootTime(SYSTEMTIME& lastBootTime) {
-    // Get the tick count (milliseconds) since the system was started
-    ULONGLONG elapsedTime = GetTickCount64();
+// Function to retrieve the system's last logon time
+static bool Logon(SYSTEMTIME& lastLogonTime) {
+    IWbemLocator* pLoc = nullptr;
+    IWbemServices* pSvc = nullptr;
 
-    // Get the current local time
-    GetLocalTime(&lastBootTime);
+    HRESULT hres;
 
-    // Calculate the time when the computer started
-    FILETIME ftStartTime;
-    SystemTimeToFileTime(&lastBootTime, &ftStartTime);
-    ULARGE_INTEGER startDateTime = *reinterpret_cast<ULARGE_INTEGER*>(&ftStartTime);
-    startDateTime.QuadPart -= elapsedTime * 10000; // Convert elapsed time to 100-nanosecond intervals
+    // Initialize COM with only 1 thread to avoid problems with the service name wmi query
+    hres = CoInitializeEx(0, COINIT_APARTMENTTHREADED);
+    if (FAILED(hres)) {
+        return false;
+    }
 
-    // Convert the calculated start time back to SYSTEMTIME
-    FileTimeToSystemTime(reinterpret_cast<FILETIME*>(&startDateTime), &lastBootTime);
+    hres = CoInitializeSecurity(
+        nullptr,
+        -1,
+        nullptr,
+        nullptr,
+        RPC_C_AUTHN_LEVEL_DEFAULT,
+        RPC_C_IMP_LEVEL_IMPERSONATE,
+        nullptr,
+        EOAC_NONE,
+        nullptr
+    );
+
+    if (FAILED(hres)) {
+        CoUninitialize();
+        return false;
+    }
+
+    // Obtain the initial locator to WMI
+    hres = CoCreateInstance(
+        CLSID_WbemLocator,
+        0,
+        CLSCTX_INPROC_SERVER,
+        IID_IWbemLocator,
+        reinterpret_cast<LPVOID*>(&pLoc)
+    );
+
+    if (FAILED(hres)) {
+        CoUninitialize();
+        return false;
+    }
+
+    // Connect to WMI through the IWbemLocator::ConnectServer method
+    hres = pLoc->ConnectServer(
+        _bstr_t(L"ROOT\\CIMV2"),
+        nullptr,
+        nullptr,
+        0,
+        0,
+        0,
+        0,
+        &pSvc
+    );
+
+    if (FAILED(hres)) {
+        pLoc->Release();
+        CoUninitialize();
+        return false;
+    }
+
+    hres = CoSetProxyBlanket(
+        pSvc,
+        RPC_C_AUTHN_WINNT,
+        RPC_C_AUTHZ_NONE,
+        nullptr,
+        RPC_C_AUTHN_LEVEL_CALL,
+        RPC_C_IMP_LEVEL_IMPERSONATE,
+        nullptr,
+        EOAC_NONE
+    );
+
+    if (FAILED(hres)) {
+        pSvc->Release();
+        pLoc->Release();
+        CoUninitialize();
+        return false;
+    }
+
+    IEnumWbemClassObject* pEnumerator = nullptr;
+
+    hres = pSvc->ExecQuery(
+        _bstr_t("WQL"),
+        _bstr_t("SELECT * FROM Win32_LogonSession WHERE LogonType = 2"),
+        WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
+        nullptr,
+        &pEnumerator
+    );
+
+    if (FAILED(hres)) {
+        return false;
+    }
+
+    FILETIME maxLogonFileTime = {};
+    bool isFirstLogon = true;
+
+    while (pEnumerator) {
+        IWbemClassObject* pclsObj = nullptr;
+        ULONG uReturn = 0;
+
+        hres = pEnumerator->Next(WBEM_INFINITE, 1, &pclsObj, &uReturn);
+
+        if (uReturn == 0) {
+            break;
+        }
+
+        VARIANT vtProp;
+        VariantInit(&vtProp);
+
+        // Use the previously assigned hres
+        if (SUCCEEDED(hres)) {
+            hres = pclsObj->Get(L"StartTime", 0, &vtProp, 0, 0);
+
+            if (SUCCEEDED(hres) && vtProp.vt == VT_BSTR) {
+                SYSTEMTIME logonTime{};
+                swscanf_s(vtProp.bstrVal, L"%4hd%2hd%2hd%2hd%2hd%2hd",
+                    &logonTime.wYear, &logonTime.wMonth, &logonTime.wDay,
+                    &logonTime.wHour, &logonTime.wMinute, &logonTime.wSecond);
+
+                FILETIME logonFileTime;
+                SystemTimeToFileTime(&logonTime, &logonFileTime);
+
+                if (isFirstLogon || CompareFileTime(&logonFileTime, &maxLogonFileTime) > 0) {
+                    maxLogonFileTime = logonFileTime;
+                    lastLogonTime = logonTime;
+                    isFirstLogon = false;
+                }
+            }
+        }
+
+        VariantClear(&vtProp);
+        pclsObj->Release();
+    }
+
+    // Release pEnumerator only if it is not null just to avoid compiler warnings
+    if (pEnumerator) {
+        pEnumerator->Release();
+    }
+
+    if (isFirstLogon) {
+        return false;
+    }
+
+    CoUninitialize();
+
+    return true;
 }
 
 // Function to convert FILETIME to a local time string
@@ -157,7 +288,7 @@ void UnpluggedDevices() {
 
     // Get the system's last boot time
     SYSTEMTIME lastBootTime{};
-    LastComputerBootTime(lastBootTime);
+    Logon(lastBootTime);
 
     // Call EnumerateSubkeys with the last boot time and one level depth (so that we can detect unplugged usbs)
     EnumerateSubkeys(parentKeyPath, lastBootTime, 1);
