dbbaa02dc4902e686c805087aef96cd1a8af340c
Requiem <114197630+NotRequiem@users.noreply.github.com>
2024-02-04 13:03:41 +0100

    Update color.h
    Signed-off-by: Requiem <114197630+NotRequiem@users.noreply.github.com>

--- Screenshare Tool/miscellaneous/gui/color.h
+++ Screenshare Tool/miscellaneous/gui/color.h
@@ -24,7 +24,7 @@ extern "C" {
         BrightYellow,
         BrightWhite,
 
-        // Additional colors
+        // Additional foreground colors
         DarkGray = 8,
         DarkBlue,
         DarkGreen,
@@ -39,12 +39,32 @@ extern "C" {
         LightRed,
         LightMagenta,
         LightYellow,
-        LightWhite
+        LightWhite,
+
+        // Background colors
+        BGBlack = 0x0000,
+        BGBlue = 0x0010,
+        BGGreen = 0x0020,
+        BGCyan = 0x0030,
+        BGRed = 0x0040,
+        BGMagenta = 0x0050,
+        BGYellow = 0x0060,
+        BGWhite = 0x0070,
+        BGGray = 0x0080,
+        BGBrightBlue = 0x0090,
+        BGBrightGreen = 0x00A0,
+        BGBrightCyan = 0x00B0,
+        BGBrightRed = 0x00C0,
+        BGBrightMagenta = 0x00D0,
+        BGBrightYellow = 0x00E0,
+        BGBrightWhite = 0x00F0,
     };
 
     // Function prototypes to include color in the ss tool
     void setConsoleTextColor(enum ConsoleColor textColor);
     void resetConsoleTextColor();
+
+#define FOREGROUND_RESET (FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE)
 
 #ifdef __cplusplus
 }
