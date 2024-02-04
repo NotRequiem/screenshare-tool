9f343fab6caa93c3028efeb20791ec20444e7bd0
Requiem <114197630+NotRequiem@users.noreply.github.com>
2024-02-04 12:16:35 +0100

    Update bam.cpp
    Signed-off-by: Requiem <114197630+NotRequiem@users.noreply.github.com>

--- Screenshare Tool/checks/bam/bam.cpp
+++ Screenshare Tool/checks/bam/bam.cpp
@@ -23,14 +23,6 @@ static bool IsFileSignatureValid(const std::wstring& filePath) {
     return wrapper.VerifyFileSignature(filePath);
 }
 
-// Function to convert std::wstring to UTF-8 std::string
-static std::string convertWStringToUtf8(const std::wstring& wstr) {
-    int utf8Length = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, nullptr, 0, nullptr, nullptr);
-    std::string utf8String(utf8Length, 0);
-    WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, &utf8String[0], utf8Length, nullptr, nullptr);
-    return utf8String;
-}
-
 static std::wstring ConvertDevicePathToFilePath(const std::wstring& devicePath)
 {
     static std::map<std::wstring, std::wstring> dosPathDevicePathMap = GetDosPathDevicePathMap();
