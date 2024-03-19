a446b5b54e0796bfeeff8f3dc7dbba48ac1dc4c4
Requiem <114197630+NotRequiem@users.noreply.github.com>
2024-03-19 04:46:24 +0100

    Update bam.cpp
    Signed-off-by: Requiem <114197630+NotRequiem@users.noreply.github.com>

--- Screenshare Tool/checks/bam/bam.cpp
+++ Screenshare Tool/checks/bam/bam.cpp
@@ -1,41 +1,9 @@
 #include "bam.hpp"
 
-static std::map<std::wstring, std::wstring> GetDosPathDevicePathMap()
-{
-    wchar_t devicePath[MAX_PATH] = { 0 };
-    std::map<std::wstring, std::wstring> result;
-    std::wstring dosPath = L"A:";
-
-    for (wchar_t letter = L'A'; letter <= L'Z'; ++letter)
-    {
-        dosPath[0] = letter;
-        if (QueryDosDeviceW(dosPath.c_str(), devicePath, MAX_PATH))
-        {
-            result[dosPath] = devicePath;
-        }
-    }
-    return result;
-}
-
 // Function to check if a file signature is valid
 static bool IsFileSignatureValid(const std::wstring& filePath) {
     TrustVerifyWrapper wrapper;
     return wrapper.VerifyFileSignature(filePath);
-}
-
-static std::wstring ConvertDevicePathToFilePath(const std::wstring& devicePath)
-{
-    static std::map<std::wstring, std::wstring> dosPathDevicePathMap = GetDosPathDevicePathMap();
-
-    for (const auto& mapping : dosPathDevicePathMap)
-    {
-        if (devicePath.find(mapping.second) == 0)
-        {
-            return mapping.first + devicePath.substr(mapping.second.length());
-        }
-    }
-
-    return devicePath; // Return original if no match is found
 }
 
 void ListBinaryRegistryValues(HKEY hKey, const wchar_t* subKey);
