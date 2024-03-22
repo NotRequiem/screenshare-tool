c246d23a23668d3d21787a700459c5aad5b6f932
Requiem <114197630+NotRequiem@users.noreply.github.com>
2024-03-22 17:02:46 +0100

    Update userproc.cpp
    Signed-off-by: Requiem <114197630+NotRequiem@users.noreply.github.com>

--- Screenshare Tool/checks/memory/userproc.cpp
+++ Screenshare Tool/checks/memory/userproc.cpp
@@ -1,11 +1,158 @@
 #include "userproc.hpp"
 
 std::unordered_set<std::wstring> printedLines;
+static std::vector<std::string> extensions = { ".exe", ".dll", ".jar", ".bat", ".vbs", ".py", ".ps1" };
 
 // Function to check if a file signature is valid
 static bool IsFileSignatureValid(const std::wstring& filePath) {
     TrustVerifyWrapper wrapper;
     return wrapper.VerifyFileSignature(filePath);
+}
+
+// Utility function to replace all occurrences of a character in a string
+void ReplaceAll(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = 0;
+    while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length();
+    }
+}
+
+static void ExtractFilePath(const std::string& line) {
+    size_t startPos = std::string::npos;
+    size_t endPos = std::string::npos;
+
+    if (line.find("TRACE,0000,0000,PcaClient,MonitorProcess,") == 0) {
+        startPos = line.find("TRACE,0000,0000,PcaClient,MonitorProcess,") + 41; // Move past "TRACE,0000,0000,PcaClient,MonitorProcess,"
+    }
+    else if (line.find("file:///") == 0) {
+        startPos = line.find("file:///") + 8; // Move past "file:///"
+    }
+
+    // Find the extension
+    for (const auto& ext : extensions) {
+        size_t foundPos = line.find(ext, startPos);
+        if (foundPos != std::string::npos) {
+            endPos = foundPos + ext.size();
+            break;
+        }
+    }
+
+    if (startPos != std::string::npos && endPos != std::string::npos) {
+        // Remove any characters after the extension
+        std::string filePath = line.substr(startPos, endPos - startPos);
+        // Replace forward slashes with backslashes
+        size_t pos = 0;
+        while ((pos = filePath.find("%20", pos)) != std::string::npos) {
+            filePath.replace(pos, 3, " ");
+            pos += 1; // Move past the replaced space to avoid infinite loop
+        }
+        ReplaceAll(filePath, "/", "\\");
+        std::wstring wideFilePath(filePath.begin(), filePath.end());
+        if (printedLines.find(wideFilePath) == printedLines.end()) {
+            if (fs::exists(wideFilePath)) {
+                if (!IsFileSignatureValid(wideFilePath)) {
+                    std::wcout << L"[#] Executed & Unsigned file: " << wideFilePath << std::endl;
+                }
+            }
+            else {
+                std::wcout << L"[#] Executed & Deleted file: " << wideFilePath << std::endl;
+            }
+            printedLines.insert(wideFilePath);
+        }
+    }
+}
+
+DWORD GetExplorerPID() {
+    DWORD processes[1024], cbNeeded, cProcesses;
+    if (!EnumProcesses(processes, sizeof(processes), &cbNeeded)) {
+        return 0;
+    }
+
+    cProcesses = cbNeeded / sizeof(DWORD);
+
+    for (DWORD i = 0; i < cProcesses; ++i) {
+        DWORD pid = processes[i];
+        HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid);
+        if (hProcess != NULL) {
+            WCHAR processName[MAX_PATH];
+            if (GetProcessImageFileNameW(hProcess, processName, sizeof(processName) / sizeof(WCHAR))) {
+                std::wstring processNameStr(processName);
+                std::transform(processNameStr.begin(), processNameStr.end(), processNameStr.begin(), ::tolower);
+                if (processNameStr.find(L"explorer.exe") != std::wstring::npos) {
+                    CloseHandle(hProcess);
+                    return pid;
+                }
+            }
+            CloseHandle(hProcess);
+        }
+    }
+    return 0;
+}
+
+static void Explorer() {
+    // Get the PID of the Explorer process
+    DWORD pid = GetExplorerPID();
+    if (pid == 0) {
+        std::cerr << "Failed to find explorer.exe process." << std::endl;
+        return;
+    }
+
+    // Build command line to execute the memory scanner
+    std::wstring commandLine = L"memory.exe -p " + std::to_wstring(pid);
+    // Create pipe handles with proper memory management using smart pointers
+    HANDLE hChildStdoutRd, hChildStdoutWr;
+    SECURITY_ATTRIBUTES saAttr{};
+    saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+    saAttr.bInheritHandle = TRUE;
+    saAttr.lpSecurityDescriptor = NULL;
+
+    if (!CreatePipe(&hChildStdoutRd, &hChildStdoutWr, &saAttr, 0)) {
+        return;
+    }
+
+    // Set up STARTUPINFO and PROCESS_INFORMATION structs
+    STARTUPINFOW si = { sizeof(STARTUPINFOW) };
+    si.hStdOutput = hChildStdoutWr;
+    si.dwFlags |= STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
+    si.wShowWindow = SW_HIDE;  // Hide the console window
+
+    PROCESS_INFORMATION pi;
+
+    // Launch the memory scanner process
+    if (!CreateProcessW(NULL, const_cast<wchar_t*>(commandLine.c_str()), NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi)) {
+        CloseHandle(hChildStdoutRd);
+        CloseHandle(hChildStdoutWr);
+        return;
+    }
+
+    // Close unnecessary write handle
+    CloseHandle(hChildStdoutWr);
+
+    // Read and process the output of the memory scanner
+    CHAR buffer[4096]{};
+    DWORD bytesRead;
+    while (ReadFile(hChildStdoutRd, buffer, sizeof(buffer), &bytesRead, NULL) && bytesRead != 0) {
+        std::string outputString(buffer, bytesRead);
+
+        // Use istringstream to parse the output into lines
+        std::istringstream outputStream(outputString);
+        std::string line;
+        while (std::getline(outputStream, line)) {
+            if ((line.find("TRACE,0000,0000,PcaClient,MonitorProcess,") == 0 || line.find("file:///") == 0) && line.find(".exe") != std::string::npos) {
+                if ((line.find("TRACE,0000,0000,PcaClient,MonitorProcess,") == 0 || line.find("file:///") == 0) && line.find(".exe") != std::string::npos) {
+                    ExtractFilePath(line);
+                }
+            }
+        }
+    }
+
+
+    // Wait for the process to finish and close handles
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+    CloseHandle(hChildStdoutRd);
 }
 
 static void DetectExes(DWORD pid) {
@@ -182,6 +329,12 @@ void ExecutedFiles(bool imp) {
         resetConsoleTextColor();
     }
 
+    // Print the process being scanned
+    setConsoleTextColor(BrightYellow);
+    std::wcout << "[Memory Scanner] Analyzing process " << "Explorer" << std::endl;
+    resetConsoleTextColor();
+    Explorer();
+
     IWbemLocator* pLoc = nullptr;
     IWbemServices* pSvc = nullptr;
     HRESULT hr = InitializeWMI(pLoc, pSvc);
@@ -215,7 +368,7 @@ void ExecutedFiles(bool imp) {
                     if (serviceConfig != nullptr) {
                         if (QueryServiceConfig(service, serviceConfig, bytesNeeded, &bytesNeeded) != 0) {
                             if (serviceConfig->dwStartType == SERVICE_DISABLED) {
-                                std::wcerr << L"[!] The following service is disabled: '" << serviceName << L"'. Ban the user.\n";
+                                std::wcerr << L"[#] The following service is disabled: '" << serviceName << L"'. This is not considered bannable.\n";
                                 CloseServiceHandle(service);
                                 CloseServiceHandle(scm);
                                 free(serviceConfig);
