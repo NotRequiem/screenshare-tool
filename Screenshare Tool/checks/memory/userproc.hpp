58221de3bb05d96cabed20b22faa60f4d3b36054
Requiem <114197630+NotRequiem@users.noreply.github.com>
2024-03-22 16:54:09 +0100

    Update userproc.hpp
    Signed-off-by: Requiem <114197630+NotRequiem@users.noreply.github.com>

--- Screenshare Tool/checks/memory/userproc.hpp
+++ Screenshare Tool/checks/memory/userproc.hpp
@@ -7,10 +7,13 @@
 #include <unordered_set>
 #include <map>
 #include <filesystem>
+#include <psapi.h>
 
 #include "..\..\miscellaneous\wmi\wmi.hpp"
 #include "..\..\miscellaneous\gui\color.h"
 #include "..\..\miscellaneous\digital signature\trustverify.hpp"
 #include "..\..\miscellaneous\device\device.hpp"
+
+namespace fs = std::filesystem;
 
 void ExecutedFiles(bool imp);
