76294c98301595e9b90a0b1775310c12a74a5c1e
Requiem <114197630+NotRequiem@users.noreply.github.com>
2024-03-23 00:26:52 +0100

    Update main.cpp
    Signed-off-by: Requiem <114197630+NotRequiem@users.noreply.github.com>

--- Screenshare Tool/main.cpp
+++ Screenshare Tool/main.cpp
@@ -1,126 +1,371 @@
 #include "main.hpp"
 
+namespace Checks {
+    class Check {
+    public:
+        virtual void runCheck(bool imp) = 0;
+        virtual ~Check() {}
+    };
+
+    class MacroStringsCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            std::cout << "Running Macro Strings Check." << std::endl;
+
+        }
+    };
+
+    class ExecutedFilesCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            ExecutedFiles(imp);
+        }
+    };
+
+    class USNJournalClearedCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            USNJournalCleared(imp);
+        }
+    };
+
+    class SystemTimeChangeCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+#ifdef _WIN64
+            SystemTimeChange(imp);
+#endif
+        }
+    };
+
+    class SystemInformerCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            SystemInformer(imp);
+
+        }
+    };
+
+    class RestartedProcessesCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            RestartedProcesses(imp);
+
+        }
+    };
+
+    class EventlogBypassCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            EventlogBypass(imp);
+
+        }
+    };
+
+    class LocalHostCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            LocalHost();
+
+        }
+    };
+
+    class BamCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            bam(imp);
+
+        }
+    };
+
+    class SuspiciousModsCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            SuspiciousMods();
+
+        }
+    };
+
+    class ReplacedDisksCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            ReplacedDisks(imp);
+
+        }
+    };
+
+    class TaskSchedulerCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            TaskScheduler(imp);
+
+        }
+    };
+
+    class PrefetchCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            Prefetch(imp);
+
+        }
+    };
+
+    class RecentFilesCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            RecentFiles(imp);
+
+        }
+    };
+
+    class AppCrashCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            AppCrash(imp);
+
+        }
+    };
+
+    class JavawCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            Javaw();
+        }
+    };
+
+    class XRayCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            XRay(imp);
+
+        }
+    };
+
+    class ImportCodeCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            ImportCode(imp);
+
+        }
+    };
+
+    class UnpluggedDevicesCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            UnpluggedDevices(imp);
+
+        }
+    };
+
+    class MouseKeysCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            MouseKeys();
+        }
+    };
+
+    class MiceCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            MouseCheck(imp);
+
+        }
+    };
+
+    class VirtualMachineCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            VirtualMachine(imp);
+
+        }
+    };
+
+    class CsrssCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            csrss(imp);
+
+        }
+    };
+
+    class USNJournalCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            USNJournal(imp);
+
+        }
+    };
+
+    class MacrosCheck : public Check {
+    public:
+        void runCheck(bool imp) override {
+            Macros();
+        }
+    };
+}
+
 int main(int argc, char* argv[]) {
     // ================================================================================================
     //                                    SCREENSHARE TOOL INITIALIZATION
     // ================================================================================================
-      
-    // Check command line arguments to know if the ss tool should print important information
-    for (int i = 1; i < argc; ++i) {
-        if (strcmp(argv[i], "-i") == 0 || strcmp(argv[i], "-I") == 0) {
-            imp = true;
-        }
+
+    using namespace Checks;
+
+    // Set the locale for wide character support
+    if (setlocale(LC_ALL, ".UTF-8") == nullptr) {
+        std::cerr << "Failed to set UTF-8 locale." << std::endl;
     }
 
     CheckProcessorArchitecture();
     checkMemoryExe(); // Checks if the memory scanner is present
-    setlocale(LC_ALL, "");  // Set the locale for wide character support
 
-    // ================================================================================================
-    //                                    SCREENSHARE TOOL CHECKS
-    // ================================================================================================
+    std::vector<std::shared_ptr<Check>> checks = []() {
+        return std::vector<std::shared_ptr<Check>> {
+            std::make_shared<MacroStringsCheck>(),
+                std::make_shared<ExecutedFilesCheck>(),
+                std::make_shared<USNJournalClearedCheck>(),
+                std::make_shared<SystemTimeChangeCheck>(),
+                std::make_shared<SystemInformerCheck>(),
+                std::make_shared<RestartedProcessesCheck>(),
+                std::make_shared<EventlogBypassCheck>(),
+                std::make_shared<LocalHostCheck>(),
+                std::make_shared<BamCheck>(),
+                std::make_shared<SuspiciousModsCheck>(),
+                std::make_shared<ReplacedDisksCheck>(),
+                std::make_shared<TaskSchedulerCheck>(),
+                std::make_shared<PrefetchCheck>(),
+                std::make_shared<RecentFilesCheck>(),
+                std::make_shared<AppCrashCheck>(),
+                std::make_shared<JavawCheck>(),
+                std::make_shared<XRayCheck>(),
+                std::make_shared<ImportCodeCheck>(),
+                std::make_shared<UnpluggedDevicesCheck>(),
+                std::make_shared<MouseKeysCheck>(),
+                std::make_shared<MiceCheck>(),
+                std::make_shared<VirtualMachineCheck>(),
+                std::make_shared<CsrssCheck>(),
+                std::make_shared<USNJournalCheck>(),
+                std::make_shared<MacrosCheck>()
+        };
+        }();
 
-    MacroStrings(imp); // Checks for macro strings. This should run first because macro strings are fastly erased
+        auto findImpFlag = [&argc, &argv]() {
+            return std::find_if(argv, argv + argc, [](const char* arg) {
+                return std::strcmp(arg, "-i") == 0 || std::strcmp(arg, "-I") == 0;
+                }) != argv + argc; // Return true if -i or -I is found, false otherwise
+            };
 
-    ExecutedFiles(imp); // Checks executed files using several processes
+        bool imp = findImpFlag(); // Get the boolean value from findImpFlag()
 
-    USNJournalCleared(imp); // Check if USNJournal was cleared
+        for (const auto& check : checks) {
+            check->runCheck(imp); // Pass the boolean value to runCheck
+        }
 
-#ifdef _WIN64
-    SystemTimeChange(imp); // Checks if the system time was changed
-#endif
+        // ================================================================================================
+        //                                    SCREENSHARE TOOL CHECKS
+        // ================================================================================================
 
-    SystemInformer(imp); // Checks if System Informer or Process Hacker were executed
+        /*
+        MacroStrings(imp); // Checks for macro strings. This should run first because macro strings are fastly erased
 
-    RestartedProcesses(imp); // Checks if certain processes needed to check for certain bypasses and file executions are restarted
+        ExecutedFiles(imp); // Checks executed files using several processes
 
-    EventlogBypass(imp); // Checks if eventlog was bypassed
+        USNJournalCleared(imp); // Check if USNJournal was cleared
 
-    LocalHost(); // Checks if there is a networh shared drive that could contain cheat files
+        SystemTimeChange(imp); // Checks if the system time was changed
 
-    bam(imp); // Checks executed files with the Background Activity Moderator
+        SystemInformer(imp); // Checks if System Informer or Process Hacker were executed
 
-    SuspiciousMods(); // Checks for mods that were modified while Minecraft was running
+        RestartedProcesses(imp); // Checks if certain processes needed to check for certain bypasses and file executions are restarted
 
-    ReplacedDisks(imp); // Checks physical or virtual disks replaced or formatted before the Screenshare
+        EventlogBypass(imp); // Checks if eventlog was bypassed
 
-    TaskScheduler(imp); // Checks bypasses using Task Scheduler
+        LocalHost(); // Checks if there is a networh shared drive that could contain cheat files
 
-    Prefetch(imp); // Checks executed files with Prefetch
+        bam(imp); // Checks executed files with the Background Activity Moderator
 
-    RecentFiles(imp); // Checks recently accessed files.
+        SuspiciousMods(); // Checks for mods that were modified while Minecraft was running
 
-    AppCrash(imp); // Checks executed (and crashed) files with WER
+        ReplacedDisks(imp); // Checks physical or virtual disks replaced or formatted before the Screenshare
 
-    Javaw(); // Checks unlegit clients and mods using Minecraft's memory
+        TaskScheduler(imp); // Checks bypasses using Task Scheduler
 
-    XRay(imp); // Checks xray resource packs
+        Prefetch(imp); // Checks executed files with Prefetch
 
-    ImportCode(imp); // Checks bypasses using code imports on system terminals
+        RecentFiles(imp); // Checks recently accessed files.
 
-    UnpluggedDevices(imp); // Checks unplugged devices
+        AppCrash(imp); // Checks executed (and crashed) files with WER
 
-    MouseKeys(); // Checks if autoclickers using MouseKeys are enabled.
+        Javaw(); // Checks unlegit clients and mods using Minecraft's memory
 
-    MouseCheck(imp); // Checks the VID and PID of the mouse being used.
+        XRay(imp); // Checks xray resource packs
 
-    VirtualMachine(imp); // Checks if the user is using a Virtual Machine
+        ImportCode(imp); // Checks bypasses using code imports on system terminals
 
-    csrss(imp); // Checks execution of unsigned files with modified extensions, unsigned executed files and unsigned injected dlls
+        UnpluggedDevices(imp); // Checks unplugged devices
 
-    USNJournal(imp); // Checks certain file modifications, such as macro modifications, replaced files and special characters
+        MouseKeys(); // Checks if autoclickers using MouseKeys are enabled.
 
-    Macros(); // Checks for macro files modifications. This should run at the end to counter macro switch profile bypasses
+        MouseCheck(imp); // Checks the VID and PID of the mouse being used.
 
-    // ------------------------------------------------------------------------------------------------
-    //                                  ONBOARD MEMORY MACROS CHECKS
-    // ------------------------------------------------------------------------------------------------
+        VirtualMachine(imp); // Checks if the user is using a Virtual Machine
 
-    STARTUPINFO si = { sizeof(si) };
-    PROCESS_INFORMATION pi;
+        csrss(imp); // Checks execution of unsigned files with modified extensions, unsigned executed files and unsigned injected dlls
 
-    if (!CreateProcess(NULL,   // Module name (NULL to use the same module as the calling process)
-        NULL,                   // Command line (NULL to indicate no command line arguments)
-        NULL,                   // Process handle not inheritable
-        NULL,                   // Thread handle not inheritable
-        FALSE,                  // Set handle inheritance to FALSE
-        CREATE_NEW_CONSOLE,     // Create a new console window for the new process
-        NULL,                   // Use parent's environment block
-        NULL,                   // Use parent's starting directory 
-        &si,                    // Pointer to STARTUPINFO structure
-        &pi))                   // Pointer to PROCESS_INFORMATION structure
-    {
-        // If the process creation fails, fallback to AllocConsole
-        if (AllocConsole()) {
-            FILE* pCout, * pCerr;
-            if (freopen_s(&pCout, "CONOUT$", "w", stdout) != 0) {
-                MessageBox(NULL, "Failed to redirect stdout", "Error", MB_OK | MB_ICONERROR);
-                return 1;
+        USNJournal(imp); // Checks certain file modifications, such as macro modifications, replaced files and special characters
+
+        Macros(); // Checks for macro files modifications. This should run at the end to counter macro switch profile bypasses
+        */
+
+        // ------------------------------------------------------------------------------------------------
+        //                                  ONBOARD MEMORY MACROS CHECKS
+        // ------------------------------------------------------------------------------------------------
+
+        STARTUPINFO si = { sizeof(si) };
+        PROCESS_INFORMATION pi;
+
+        if (!CreateProcess(NULL,   // Module name (NULL to use the same module as the calling process)
+            NULL,                   // Command line (NULL to indicate no command line arguments)
+            NULL,                   // Process handle not inheritable
+            NULL,                   // Thread handle not inheritable
+            FALSE,                  // Set handle inheritance to FALSE
+            CREATE_NEW_CONSOLE,     // Create a new console window for the new process
+            NULL,                   // Use parent's environment block
+            NULL,                   // Use parent's starting directory 
+            &si,                    // Pointer to STARTUPINFO structure
+            &pi))                   // Pointer to PROCESS_INFORMATION structure
+        {
+            // If the process creation fails, fallback to AllocConsole
+            if (AllocConsole()) {
+                FILE* pCout, * pCerr;
+                if (freopen_s(&pCout, "CONOUT$", "w", stdout) != 0) {
+                    MessageBox(NULL, "Failed to redirect stdout", "Error", MB_OK | MB_ICONERROR);
+                    return 1;
+                }
+                if (freopen_s(&pCerr, "CONOUT$", "w", stderr) != 0) {
+                    MessageBox(NULL, "Failed to redirect stderr", "Error", MB_OK | MB_ICONERROR);
+                    return 1;
+                }
+
+                // Execute hook code
+                ExecuteHookCode();
+
+                // Free the console
+                FreeConsole();
             }
-            if (freopen_s(&pCerr, "CONOUT$", "w", stderr) != 0) {
-                MessageBox(NULL, "Failed to redirect stderr", "Error", MB_OK | MB_ICONERROR);
-                return 1;
+            else {
+                // If AllocConsole also fails, execute the hook code without a console
+                ExecuteHookCode();
             }
-
-            // Execute hook code
-            ExecuteHookCode();
-
-            // Free the console
-            FreeConsole();
         }
         else {
-            // If AllocConsole also fails, execute the hook code without a console
+            // Execute the hook code within the new process
             ExecuteHookCode();
+
+            // Close process and thread handles
+            CloseHandle(pi.hProcess);
+            CloseHandle(pi.hThread);
         }
-    }
-    else {
-        // Execute the hook code within the new process
-        ExecuteHookCode();
 
-        // Close process and thread handles
-        CloseHandle(pi.hProcess);
-        CloseHandle(pi.hThread);
-    }
-
-    return 0;
+        return 0;
 }
